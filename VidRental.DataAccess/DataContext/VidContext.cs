e634bd7bb10daa284a7a58ae674821a282232a4a
Enaris <bularzkamil@gmail.com>
2020-04-05 09:41:12 +0200

    Add project files.

---
+++ VidRental.DataAccess/DataContext/VidContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VidRental.DataAccess.DbModels;
+
+namespace VidRental.DataAccess.DataContext
+{
+    public class VidContext : IdentityDbContext<User>
+    {
+        public VidContext(DbContextOptions<VidContext> options) : base(options)
+        {
+
+        }
+    }
+}
